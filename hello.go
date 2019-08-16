13b26fbade087bf5e2d4f8506e17354a5026cd5d
waspryo <wasp2123321@gmail.com>
2019-08-16 18:48:19 +0900

    map

--- hello.go
+++ hello.go
@@ -5,7 +5,17 @@ import (
 )
 
 func main() {
-	a := []int{5, 4, 3, 2, 1}
-	a = append(a, 13)
-	fmt.Println(a)
+	// a := []int{5, 4, 3, 2, 1}
+	// a = append(a, 13)
+	// fmt.Println(a)
+
+	vertices := make(map[string]int)
+
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	delete(vertices, "square")
+
+	fmt.Println(vertices)
 }
